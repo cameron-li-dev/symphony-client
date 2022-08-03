8703bc4bf202183113ac396f85a013be2eeed4ad
cali <cameron.li@trademe.co.nz>
2022-08-03 15:47:56 +1200

    Stylized

--- src/components/crosshair-card/crosshair-service.tsx
+++ src/components/crosshair-card/crosshair-service.tsx
@@ -178,3 +178,178 @@ export const importCrosshair = (config: string) => {
 
     return imported
 }
+
+export const exportCrosshair = (config: ICrosshair) => {
+    const base: ICrosshair = JSON.parse(JSON.stringify(defaultCrosshair));
+    let exportedString = "0";
+    if (isCrosshairsEqual(config, base)) {
+        return exportedString;
+    }
+    exportedString = exportedString + ";P;";
+
+    // show outlines: h;0 - turn off [default = on]
+    if (!config.config.outlines.outlinesEnabled) {
+        exportedString = exportedString + "h;0;"
+    } else {
+        // outline opacity: o;float
+        if (config.config.outlines.outline.outlineOpacity !== base.config.outlines.outline.outlineOpacity) {
+            exportedString = exportedString + "o;" + config.config.outlines.outline.outlineOpacity + ";";
+        }
+        // outline thickness: t;int
+        if (config.config.outlines.outline.outlineThickness !== base.config.outlines.outline.outlineThickness) {
+            exportedString = exportedString + "t;" + config.config.outlines.outline.outlineThickness + ";";
+        }
+    }
+
+    // colour - default none ;c;int
+    if (config.config.colour !== base.config.colour) {
+        exportedString = exportedString + "c;" + config.config.colour + ";";
+    }
+
+    // show center dot: d;1 - turn on [default = off]
+    if (config.config.centerDot.dotEnabled) {
+        exportedString = exportedString + "d;1;";
+
+        // center dot opacity: a;float
+        if (config.config.centerDot.dot.centerDotOpacity !== base.config.centerDot.dot.centerDotOpacity) {
+            exportedString = exportedString + "a;" + config.config.centerDot.dot.centerDotOpacity + ";";
+        }
+        // center dot thickness: z:int
+        if (config.config.centerDot.dot.centerDotThickness !== base.config.centerDot.dot.centerDotThickness) {
+            exportedString = exportedString + "z;" + config.config.centerDot.dot.centerDotThickness + ";";
+        }
+    }
+
+    // override firing error offset with crosshair offset: m;1 - turn on [default = off]
+    if (config.config.overrideOffset) {
+        exportedString = exportedString + "m;1;";
+    }
+
+    // show inner line: 0b;0 - turn off [default = on]
+    if (!config.config.innerLines.linesEnabled) {
+        exportedString = exportedString + "0b;0;";
+    } else {
+        // inner line opacity: 0a;float
+        if (config.config.innerLines.lines.lineOpacity !== base.config.innerLines.lines.lineOpacity) {
+            exportedString = exportedString + "0a;" + config.config.innerLines.lines.lineOpacity + ";";
+        }
+        // inner line length: 0l;int
+        if (config.config.innerLines.lines.lineLength !== base.config.innerLines.lines.lineLength) {
+            exportedString = exportedString + "0l;" + config.config.innerLines.lines.lineLength + ";";
+        }
+        // inner line thickness: 0t;int
+        if (config.config.innerLines.lines.lineThickness !== base.config.innerLines.lines.lineThickness) {
+            exportedString = exportedString + "0t;" + config.config.innerLines.lines.lineThickness + ";";
+        }
+        // inner line offset: 0o;int
+        if (config.config.innerLines.lines.lineOffset !== base.config.innerLines.lines.lineOffset) {
+            exportedString = exportedString + "0o;" + config.config.innerLines.lines.lineOffset + ";";
+        }
+
+        // inner line movement error: 0m;1 - turn on [default = off]
+        if (config.config.innerLines.lines.movementErrorEnabled) {
+            exportedString = exportedString + "0m;1;"
+            // inner line movement error multiplier: 0s;float
+            if (config.config.innerLines.lines.movementError.multiplier !== base.config.innerLines.lines.movementError.multiplier) {
+                exportedString = exportedString + "0s;" + config.config.innerLines.lines.movementError.multiplier + ";";
+            }
+        }
+
+        // inner line firing error: 0f;0 - turn off [default = on]
+        if (!config.config.innerLines.lines.firingErrorEnabled) {
+            exportedString = exportedString + "0f;0;"
+            // inner line movement error multiplier: 0s;float
+            if (config.config.innerLines.lines.firingError.multiplier !== base.config.innerLines.lines.firingError.multiplier) {
+                exportedString = exportedString + "0e;" + config.config.innerLines.lines.firingError.multiplier + ";";
+            }
+        }
+    }
+
+    // show inner line: 1b;0 - turn off [default = on]
+    if (!config.config.innerLines.linesEnabled) {
+        exportedString = exportedString + "1b;0;";
+    } else {
+        // inner line opacity: 1a;float
+        if (config.config.outerLines.lines.lineOpacity !== base.config.outerLines.lines.lineOpacity) {
+            exportedString = exportedString + "1a;" + config.config.outerLines.lines.lineOpacity + ";";
+        }
+        // inner line length: 1l;int
+        if (config.config.outerLines.lines.lineLength !== base.config.outerLines.lines.lineLength) {
+            exportedString = exportedString + "1l;" + config.config.outerLines.lines.lineLength + ";";
+        }
+        // inner line thickness: 1t;int
+        if (config.config.outerLines.lines.lineThickness !== base.config.outerLines.lines.lineThickness) {
+            exportedString = exportedString + "1t;" + config.config.outerLines.lines.lineThickness + ";";
+        }
+        // inner line offset: 1o;int
+        if (config.config.outerLines.lines.lineOffset !== base.config.outerLines.lines.lineOffset) {
+            exportedString = exportedString + "1o;" + config.config.outerLines.lines.lineOffset + ";";
+        }
+
+        // inner line movement error: 1m;1 - turn on [default = off]
+        if (config.config.outerLines.lines.movementErrorEnabled) {
+            exportedString = exportedString + "1m;1;"
+            // inner line movement error multiplier: 1s;float
+            if (config.config.outerLines.lines.movementError.multiplier !== base.config.outerLines.lines.movementError.multiplier) {
+                exportedString = exportedString + "1s;" + config.config.outerLines.lines.movementError.multiplier + ";";
+            }
+        }
+
+        // inner line firing error: 1f;0 - turn off [default = on]
+        if (!config.config.outerLines.lines.firingErrorEnabled) {
+            exportedString = exportedString + "1f;0;"
+            // inner line movement error multiplier: 1s;float
+            if (config.config.outerLines.lines.firingError.multiplier !== base.config.outerLines.lines.firingError.multiplier) {
+                exportedString = exportedString + "1e;" + config.config.outerLines.lines.firingError.multiplier + ";";
+            }
+        }
+    }
+
+    return exportedString;
+}
+
+export const isCrosshairsEqual = (first: ICrosshair, second: ICrosshair) => {
+    const firstConfig = first.config;
+    const secondConfig = second.config;
+
+    if (firstConfig.colour !== secondConfig.colour) return false;
+    if (firstConfig.overrideOffset !== secondConfig.overrideOffset) return false;
+
+    const firstOutlines = firstConfig.outlines;
+    const secondOutlines = secondConfig.outlines;
+    if (firstOutlines.outlinesEnabled !== secondOutlines.outlinesEnabled) return false;
+    if (firstOutlines.outline.outlineThickness !== secondOutlines.outline.outlineThickness) return false;
+    if (firstOutlines.outline.outlineOpacity !== secondOutlines.outline.outlineOpacity) return false;
+
+    const firstDot = firstConfig.centerDot;
+    const secondDot = secondConfig.centerDot;
+    if (firstDot.dotEnabled !== secondDot.dotEnabled) return false;
+    if (firstDot.dot.centerDotThickness !== secondDot.dot.centerDotThickness) return false;
+    if (firstDot.dot.centerDotOpacity !== secondDot.dot.centerDotOpacity) return false;
+
+    const firstInner = firstConfig.innerLines;
+    const secondInner = secondConfig.innerLines;
+    if (firstInner.linesEnabled !== secondInner.linesEnabled) return false;
+    if (firstInner.lines.lineLength !== secondInner.lines.lineLength) return false;
+    if (firstInner.lines.lineOffset !== secondInner.lines.lineOffset) return false;
+    if (firstInner.lines.lineOpacity !== secondInner.lines.lineOpacity) return false;
+    if (firstInner.lines.lineThickness !== secondInner.lines.lineThickness) return false;
+    if (firstInner.lines.firingErrorEnabled !== secondInner.lines.firingErrorEnabled) return false;
+    if (firstInner.lines.firingError.multiplier !== secondInner.lines.firingError.multiplier) return false;
+    if (firstInner.lines.movementErrorEnabled !== secondInner.lines.movementErrorEnabled) return false;
+    if (firstInner.lines.movementError.multiplier !== secondInner.lines.movementError.multiplier) return false;
+
+    const firstOuter = firstConfig.outerLines;
+    const secondOuter = secondConfig.outerLines;
+    if (firstOuter.linesEnabled !== secondOuter.linesEnabled) return false;
+    if (firstOuter.lines.lineLength !== secondOuter.lines.lineLength) return false;
+    if (firstOuter.lines.lineOffset !== secondOuter.lines.lineOffset) return false;
+    if (firstOuter.lines.lineOpacity !== secondOuter.lines.lineOpacity) return false;
+    if (firstOuter.lines.lineThickness !== secondOuter.lines.lineThickness) return false;
+    if (firstOuter.lines.firingErrorEnabled !== secondOuter.lines.firingErrorEnabled) return false;
+    if (firstOuter.lines.firingError.multiplier !== secondOuter.lines.firingError.multiplier) return false;
+    if (firstOuter.lines.movementErrorEnabled !== secondOuter.lines.movementErrorEnabled) return false;
+    if (firstOuter.lines.movementError.multiplier !== secondOuter.lines.movementError.multiplier) return false;
+
+    return true;
+}
--- src/pages/valorant/valorant-create/valorant-create.tsx
+++ src/pages/valorant/valorant-create/valorant-create.tsx
@@ -1,20 +1,30 @@
 import "./valorant-create.scss";
 import NavigationButton from "../../../components/button/navigation-button/navigation-button";
 import defaultCrosshair from "../../../data/default-crosshair.json";
-import React from "react";
+import React, {useEffect} from "react";
 import CrosshairCard from "../../../components/crosshair-card/crosshair-card";
 import CrosshairCreate from "../../../components/crosshair-create/crosshair-create";
+import {exportCrosshair} from "../../../components/crosshair-card/crosshair-service";
 
 export const ValorantCreate = () => {
     const [crosshair, setCrosshair] = React.useState(JSON.parse(JSON.stringify(defaultCrosshair)));
+    const [exportCode, setExportCode] = React.useState("");
+
+    useEffect(() => {
+        setExportCode(exportCrosshair(crosshair));
+    }, [crosshair]);
 
     return (
         <div className="valorant-create-container">
             <NavigationButton path="/valorant"/>
             <div className="valorant-create-content">
-                <div className="valorant-create-content--display">
-                    <CrosshairCard crosshair={crosshair}/>
+                <div>
+                    <div className="valorant-create-content--display">
+                        <CrosshairCard crosshair={crosshair}/>
+                    </div>
+                    <div>{exportCode}</div>
                 </div>
+
                 <div className="valorant-create-content--settings">
                     <CrosshairCreate crosshair={crosshair} updateCrosshair={setCrosshair}/>
                 </div>
