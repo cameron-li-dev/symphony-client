495b0f00ac9a40c3804815f5c1267f2fdbd1df8a
Cameron Li <mustemploy@outlook.com>
2022-09-04 13:22:13 +1200

    Revamping website to have main portfoliio page

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -3,13 +3,32 @@ import React from "react";
 import IFeature from "../../data/interfaces/IFeature";
 import featuresData from "../../data/features.json"
 import Carousel from "../../components/carousel/carousel";
+import CvIcon from "../../images/portfolio-rounded-square.svg";
+import LinkedInIcon from "../../images/linkedin-rounded-square.png";
+import TwitterIcon from "../../images/twitter-rounded-square.svg";
 
 export const Main = () => {
     const features: IFeature[] = JSON.parse(JSON.stringify(featuresData));
 
     return (
         <div className="main-container">
-            <Carousel displayExtra={1} features={ features }/>
+            <div className="main-content">
+                <div className="main-blurb">
+                    blurb
+                </div>
+                <div className="main--socials">
+                    <a id="cv" className="main--socials-link" href={"https://drive.google.com/file/d/19i9ADHHAguNaqNhMzdIlfOgAKMEsvF_e/view?usp=sharing"}>
+                        <img className="main--socials-icon" src={CvIcon} alt="CV"/>
+                    </a>
+                    <a id="linkedin" className="main--socials-link" href={"https://www.linkedin.com/in/cameron-li-dev/"}>
+                        <img className="main--socials-icon" src={LinkedInIcon} alt="LinkedIn"/>
+                    </a>
+                    <a id="twitter" className="main--socials-link" href={"https://twitter.com/cali_gat0r"}>
+                        <img className="main--socials-icon" src={TwitterIcon} alt="Twitter"/>
+                    </a>
+                </div>
+            </div>
+            {/*<Carousel displayExtra={1} features={ features }/>*/}
         </div>
     )
 }
