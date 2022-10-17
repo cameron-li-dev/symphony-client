e2056772845ac827e52b3b316ba74e42ff32e960
cameron.li <cameron.li@trademe.co.nz>
2022-10-17 16:29:13 +1300

    Fleshing out projects page

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -4,17 +4,18 @@ import MainBio from "../../components/main/main-bio/main-bio";
 import MainBioSocials from "../../components/main/main-bio/main-bio-socials/main-bio-socials";
 import { INavLink } from "../../data/interfaces/INavLink";
 import MainNav from "../../components/main/main-nav/main-nav";
+import ValorantLogo from "../../images/valorant-logo_brandlogos.net_kghsj.png";
 
 const navLinks: INavLink[] = [
     {
-        link: "#first",
-        text: "First",
+        link: "#main",
+        text: "Main",
         mode: "dark"
     },
     {
-        link: "#second",
-        text: "Second",
-        mode: "light"
+        link: "#projects",
+        text: "Projects",
+        mode: "dark"
     },
     {
         link: "#third",
@@ -48,10 +49,6 @@ export const Main = () => {
     }
 
     useEffect(() => {
-        // if (currentSection === 1) {
-        //     setDisplayVerticalNav(false);
-        //     setNavPosition(window.innerHeight * 1.9);
-        // } else
         if (scrollPosition > 50) {
             setDisplayVerticalNav(true);
         } else {
@@ -88,14 +85,50 @@ export const Main = () => {
                 <div className="main-content-vertical-nav" style={{display: displayVerticalNav ? 'flex' : 'none', opacity: scrollPosition > navPosition ? 1 : 0}}>
                     <MainNav horizontal={false} links={navLinks} colorMode={currentNavMode}/>
                 </div>
-                <div id="first" className="main-section">
+                <div id="main" className="main-section">
                     <MainBio links={ navLinks }/>
                     <MainBioSocials/>
                 </div>
-                <div id="second" className="main-section">
-                    <div style={{width: "100%", height: "100%", background: "black", display: "flex"}}>
-                        <div style={{color: "white", alignSelf: "center", justifySelf: "center", background: "gray", width: "100%", height: "500px", borderTop: "2px solid white", borderBottom: "2px solid white"}}>
-                            Something goes here
+                <div id="projects" className="main-section">
+                    <div className="main-projects-section">
+                        <h1 className="main-projects-section-title">
+                            Projects
+                        </h1>
+                        <div style={{width: "70%"}}>
+                            <div className="main-section__gallery">
+                                <div className="main-section__gallery-piece" style={{marginTop: "100px"}}>
+                                    <a href="/valorant">
+                                        <img src={ValorantLogo} alt={"Valorant Logo"} style={{background: "black", width: "250px", borderRadius: "16px"}}/>
+                                    </a>
+                                    <div style={{marginLeft: "16px"}}>
+                                        <h2>
+                                            Valorant Crosshairs
+                                        </h2>
+                                        <p>
+                                            Crosshair display and generator for the popular first person shooter, Valorant.
+                                        </p>
+                                        <p>
+                                            This project is created in React, SCSS, and Typescript to display and create different crosshairs.
+                                        </p>
+                                        <a href="/valorant" style={{fontSize: "16px", fontWeight: 600}}>
+                                            Visit here
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="main-section__gallery-piece" style={{flexDirection: "row-reverse", marginTop: "40px"}}>
+                                    <div style={{marginRight: "16px"}}>
+                                        <h2>
+                                            Path finding algorithms
+                                        </h2>
+                                        <p>
+                                            Table based implementation of Djikstra's and A* search path finding algorithms.
+                                        </p>
+                                        <a href="/path-find" style={{fontSize: "16px", fontWeight: 600}}>
+                                            Take a look
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
